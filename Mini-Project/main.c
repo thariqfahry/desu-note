f2d075c58130ced96de098f90b90db6c6ff9ae2b
Thariq Fahry <37890989+thariqfahry@users.noreply.github.com>
2022-03-29 15:47:11 +0100

    Create main source file
    - Created the main.c source file containing the main program entry point main().
    - Added code skeleton with main function.
    - Passed test build with no errors.

---
+++ Mini-Project/main.c
@@ -0,0 +1,12 @@
+/**
+ * main.c
+ * ELEC5620M Mini-Project program entry point
+ *
+ *  Created on: 29 March 2022
+ *      Author: Thariq Fahry (el17mtf)
+ *      SID 201158825
+ */
+
+int main(void) {
+    return 0;
+}
