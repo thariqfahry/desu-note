3c55c84bb2232c4129ef3fee0f1217bd4dcbc074
Thariq Fahry <37890989+thariqfahry@users.noreply.github.com>
2022-05-08 10:34:07 +0100

    Add graphics.
    Add graphics files for the Desu Note app.

---
+++ Mini-Project/Graphics/graphics.h
@@ -0,0 +1,12 @@
+#ifndef DN_GRAPHICS_H_
+#define DN_GRAPHICS_H_
+
+extern const unsigned short title           [76800];
+extern const unsigned short single_line_bg  [76800];
+extern const unsigned short file_bg         [76800];
+extern const unsigned short editor_bg       [76800];
+
+extern const unsigned short handwr_field    [13456];
+
+
+#endif
