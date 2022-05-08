d0f3f7ac14ec21f6f4e78c799c0793e878555836
Thariq Fahry <37890989+thariqfahry@users.noreply.github.com>
2022-05-08 10:35:10 +0100

    Add functions to launch app
    Add call to the main entry point for the Desu Note app.

--- Mini-Project/main.c
+++ Mini-Project/main.c
@@ -5,8 +5,30 @@
  *  Created on: 29 March 2022
  *      Author: Thariq Fahry (el17mtf)
  *      SID 201158825
- */
+ */ 
 
-int main(void) {
-    return 0;
+#include "DE1SoC_LT24/DE1SoC_LT24.h"
+#include "HPS_Watchdog/HPS_Watchdog.h"
+#include "CustomProjectDrivers/AD7843_TouchScreen/AD7843_TouchScreen.h"
+
+#include "DesuNote/desunote.h"
+
+//debug function if initialization fails
+void exitOnFail(signed int status, signed int successStatus){
+    if (status != successStatus) {
+        exit((int)status); //Add breakpoint here to catch failure
+    }
 }
+
+// Main program entry point
+int main(void) {
+    //Initialise LCD and touch screen
+    exitOnFail(LT24_initialise(0xFF200060,0xFF200080), LT24_SUCCESS);
+    exitOnFail(init_touch(0xFF200060), AD7843_SUCCESS);
+
+    // Launch the Desu Note app.
+    desunote();
+    
+    while (1) HPS_ResetWatchdog(); //Just reset the watchdog.
+}
+
